61f6a53d2ccef44db61d538065ee42aea87ee333
Mitchel <mitchel_jager@hotmail.com>
2026-01-01 13:45:17 +0100

    Bubble clicks in EventOnClick

--- Assets/Scripts/UI/EventOnClick.cs
+++ Assets/Scripts/UI/EventOnClick.cs
@@ -1,11 +1,27 @@
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 public class EventOnClick : MonoBehaviour, IPointerClickHandler {
     public UnityEvent assignedEvent;
+    public bool bubble = true;
+
+    void Update() {
+        if (!bubble) return;
+        if (!Mouse.current.leftButton.wasReleasedThisFrame) return;
+
+        PointerEventData eventData = new(EventSystem.current) { position = Pointer.current.position.ReadValue() };
+        System.Collections.Generic.List<RaycastResult> results = new();
+        EventSystem.current.RaycastAll(eventData, results);
+
+        if (results.Exists(r => r.gameObject.transform.IsChildOf(transform))) {
+            assignedEvent.Invoke();
+        }
+    }
 
     public void OnPointerClick(PointerEventData eventData) {
+        if (bubble) return;
         assignedEvent.Invoke();
     }
 }
