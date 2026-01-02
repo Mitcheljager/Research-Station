9ce1077362c96905a673eddacda4e15f1443aadb
Mitchel <mitchel_jager@hotmail.com>
2026-01-02 03:01:32 +0100

    Allow maximizing by double clicking drag bar

--- Assets/Scripts/UI/EventOnClick.cs
+++ Assets/Scripts/UI/EventOnClick.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
@@ -6,6 +7,10 @@ using UnityEngine.InputSystem;
 public class EventOnClick : MonoBehaviour, IPointerClickHandler {
     public UnityEvent assignedEvent;
     public bool bubble = true;
+    public bool requireDoubleClick = false;
+
+    private readonly float doubleClickPeriodSeconds = 0.4f;
+    private bool isWithinDoubleClickPeriod = false;
 
     void Update() {
         if (!bubble) return;
@@ -16,12 +21,30 @@ public class EventOnClick : MonoBehaviour, IPointerClickHandler {
         EventSystem.current.RaycastAll(eventData, results);
 
         if (results.Exists(r => r.gameObject.transform.IsChildOf(transform))) {
-            assignedEvent.Invoke();
+            Click();
         }
     }
 
     public void OnPointerClick(PointerEventData eventData) {
         if (bubble) return;
+
+        Click();
+    }
+
+    private void Click() {
+        if (requireDoubleClick && !isWithinDoubleClickPeriod) {
+            StartCoroutine(SetDoubleClickPeriod());
+            return;
+        }
+
         assignedEvent.Invoke();
+    }
+
+    private IEnumerator SetDoubleClickPeriod() {
+        isWithinDoubleClickPeriod = true;
+
+        yield return new WaitForSeconds(doubleClickPeriodSeconds);
+
+        isWithinDoubleClickPeriod = false;
     }
 }
